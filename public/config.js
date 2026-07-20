f58487d20896e5c6934797238701625c48b80d79
Jason <jasonblick@zohomail.com>
2026-07-20 19:51:32 +0000

    Activate paper trading utility (#24)

--- public/config.js
+++ public/config.js
@@ -8,5 +8,5 @@ export const APP_CONFIG = {
   allowedEmail: "jasonblick@zohomail.com",
   supabaseUrl: "https://itheknkmuutquriojdzt.supabase.co",
   supabasePublishableKey: "sb_publishable_M28yZeembNrLCMR2B1mICQ_W3Hg4mGd",
-  paperTradingEnabled: false,
+  paperTradingEnabled: true,
 };
